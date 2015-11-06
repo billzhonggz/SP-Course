4daab899e9a2631416577f747bedcf046a4b58c3
Bill ZHONG <billzhonggz@outlook.com>
2015-11-06 09:29:02 +0800

    Add comments to Assignment3_1.

--- Assignment3/Assignment3_1.c
+++ Assignment3/Assignment3_1.c
@@ -2,12 +2,16 @@
 char changeCase(char a);
 int main()
 {
+    //Get user input.
     printf("Please input a line of sentence.No longer than 100 characters in total(includes spaces).\n");
     char input[100];
-    gets(input);//Get users
+    gets(input);
     int traversalCounter;
+    //Change the first letter into uppercase if it is lowercase.
     input[0]=changeCase(input[0]);
+    //Foreach the array.
     for (traversalCounter = 0; traversalCounter < 100; traversalCounter++) {
+        //Use space to detect words. Change each letter next to the spaces to uppercase.
         if (input[traversalCounter]==' ')
             input[traversalCounter+1]=changeCase(input[traversalCounter+1]);
     }
@@ -15,7 +19,7 @@ int main()
     puts(input);
     return 0;
 }
-
+//Function to judge whether input is a lowercase letter. If it is, change it to uppercase, if it isn't, do nothing. 
 char changeCase(char a)
 {
     if (a>=97 && a<=122) {
